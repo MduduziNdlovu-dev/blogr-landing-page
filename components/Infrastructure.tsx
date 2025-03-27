9e80af9e030196c26cd471d50c432c9e35633be2
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-03-27 20:09:35 +0200

    Feat: update Infrastructure component layout and styling; add background image and global styles

--- components/Infrastructure.tsx
+++ components/Infrastructure.tsx
@@ -1,17 +1,38 @@
 import { Box, Typography } from '@mui/material'
+import Image from 'next/image'
 import React from 'react'
 
 const Infrastructure = () => {
   return (
-    <Box className="infrastructure">
-      <Box>
+    <Box className="infrastructure" sx={{
+      display:'flex',
+      flexDirection:'row',
+      justifyContent:'center',
+      alignItems:'center',
+      gap:'2rem',
+      padding:"0rem 5rem"
+    }}>
+      <Box sx={{
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+      }}>
+        <Image src="/images/illustration-phones.svg" alt='illustration-phones' width={500} height={500} />
 
       </Box>
-      <Box>
-        <Typography>
+      <Box sx={{
+        color: "white"
+      }}>
+        <Typography sx={{
+          fontSize:"1.5rem",
+          marginBottom:"1rem"
+        }}>
           State of the Art Infrastructure 
         </Typography>
-        <Typography>
+        <Typography sx={{
+          color: "hsl(206, 18.30%, 63.50%)",
+          paddingRight:"5rem"
+        }}>
           With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
         </Typography>
       </Box>
